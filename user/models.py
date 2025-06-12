a164957636ca96fdfae38ea83cd96d7d8694cf61
Angel <koriaanonimo@gmail.com>
2025-06-12 12:58:09 -0600

    AVANCE

--- user/models.py
+++ user/models.py
@@ -2,8 +2,17 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Usuario(AbstractUser):
+    ROLES = (
+        ('admin', 'Admin'),
+        ('administrador', 'Administrador'),
+        ('usuario', 'Usuario'),
+    )
+
     correo = models.EmailField(unique=True)
     nombre = models.CharField(max_length=100)
+    rol = models.CharField(max_length=20, choices=ROLES, default='usuario')
+
+    telefono=models.CharField(max_length=10, null=True, blank=True)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} ({self.rol})"
--- user/views.py
+++ user/views.py
@@ -1,17 +1,26 @@
+
 from rest_framework import viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
 from .models import Usuario
 from .serializers import UsuarioSerializer
+from .permissions import IsAdmin, IsAdministrador
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return [IsAdmin()]  # Solo los usuarios con rol 'admin' pueden listar usuarios
+        elif self.action == 'retrieve':
+            return [IsAdministrador()]  # Solo 'administrador' puede ver detalles individuales
+        return [IsAuthenticated()]  # Otros métodos requieren autenticación normal
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def current_user(request):
     serializer = UsuarioSerializer(request.user)
-    return Response(serializer.data) 
+    return Response(serializer.data)
